9b835486b93fd2644d04450fdd8550915ae83b74
Ilnur Badro <101541200+ibadro@users.noreply.github.com>
2022-10-11 15:35:17 +0500

    we

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -12,8 +12,12 @@ import java.util.List;
 @Transactional
 public class UserServiceImp implements UserService {
 
-    @Autowired
+
     private UserDao userDao;
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public void add(User user) {
